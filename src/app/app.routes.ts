16d16f40c4b4b70d8cef5c5a32e91eb509ddf71f
Ahmed Ibrahim <ahmed.aboali9874@gmail.com>
2024-05-30 13:04:19 +0300

    second

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {PayementComponent} from "./payement/payement.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path :"payement",component :PayementComponent}
+];
